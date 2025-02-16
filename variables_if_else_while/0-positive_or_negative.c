cdc86e161c41834ce82dc4beaa9c4289d53bf5d7
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-02-16 16:07:57 +0000

    positive_or_negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,24 +4,24 @@
 
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));  /* Initialize random number generator */
-    n = rand() - RAND_MAX / 2;  /* Generate random number */
+	srand(time(0));  /* Initialize random number generator */
+	n = rand() - RAND_MAX / 2;  /* Generate random number */
 
-    /* Check whether the number is positive, negative, or zero */
-    if (n > 0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else if (n == 0)
-    {
-        printf("%d is zero\n", n);
-    }
-    else
-    {
-        printf("%d is negative\n", n);
-    }
+	/* Check whether the number is positive, negative, or zero */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 
-    return 0;
+	return (0);  /* Return value wrapped in parentheses */
 }
