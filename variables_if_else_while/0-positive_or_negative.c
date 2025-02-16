cb009a880fa21ec86f13bde0eed31aed2765c39b
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-02-16 16:05:30 +0000

    positive_or_negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -6,10 +6,10 @@ int main(void)
 {
     int n;
 
-    srand(time(0));  // Initialize random number generator
-    n = rand() - RAND_MAX / 2;  // Generate random number
+    srand(time(0));  /* Initialize random number generator */
+    n = rand() - RAND_MAX / 2;  /* Generate random number */
 
-    // Check whether the number is positive, negative, or zero
+    /* Check whether the number is positive, negative, or zero */
     if (n > 0)
     {
         printf("%d is positive\n", n);
