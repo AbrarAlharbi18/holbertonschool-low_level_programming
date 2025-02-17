555bbaf406e66462edc6285d4e25b85309b6e1a7
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-02-17 13:59:52 +0000

    Add 1-alphabet.c program

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,19 +3,15 @@
 /**
  * print_alphabet - prints the alphabet in lowercase
  *
- * Description: Uses _putchar twice only
+ * Return: nothing
  */
 void print_alphabet(void)
 {
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-	int i = 2;
+	char alphabet;
 
-	_putchar(alphabet[0]);
-	_putchar(alphabet[1]);
-
-	while (alphabet[i] != '\0')
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		_putchar(alphabet[i]);
-		i++;
+		_putchar(alphabet);
 	}
+	_putchar('\n');
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+void print_alphabet(void);
+void _putchar(char c);
 
 #endif /* MAIN_H */
