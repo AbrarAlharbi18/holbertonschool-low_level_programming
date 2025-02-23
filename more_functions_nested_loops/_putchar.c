ab6cfa1010710d71bdea7e1914c24053de6c3555
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-02-23 20:43:46 +0000

    new change

--- more_functions_nested_loops/_putchar.c
+++ more_functions_nested_loops/_putchar.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes a character to stdout
+ * _putchar - writes a character to standard output
  * @c: The character to print
  *
- * Return: On success, 1 is returned. On error, -1 is returned.
+ * Return: On success 1, on error -1 is returned and errno is set appropriately.
  */
 int _putchar(char c)
 {
