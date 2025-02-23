4e6a14ddbec41120fec9c3e42905a5b1cef10c91
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-02-23 19:46:28 +0000

    task 4

---
+++ more_functions_nested_loops/4-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_most_numbers();  /* Call the function to print numbers */
+    return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_numbers(void);
+int _putchar(char c);          /* Prototype for _putchar function */
+void print_most_numbers(void); /* Prototype for print_most_numbers function */
 
 #endif /* MAIN_H */
