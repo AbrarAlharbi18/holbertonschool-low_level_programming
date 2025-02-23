ff4512f258c92e81fb01390b0004661ec265b2e1
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-02-23 20:47:49 +0000

    betty check

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,58 +1,34 @@
 #include "main.h"
 
-/**
- * main - Entry point to the program
- *
- * Return: Always 0
- */
+void fizz_buzz(void); /* Declare the fizz_buzz function before main */
+
 int main(void)
 {
-	fizz_buzz();
-	return (0);
+    fizz_buzz();  /* Call the fizz_buzz function */
+    return 0;
 }
 
-/**
- * fizz_buzz - prints numbers 1 to 100 with "Fizz", "Buzz", or "FizzBuzz"
- * based on the conditions.
- */
 void fizz_buzz(void)
 {
-	int i;
-
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-		}
-		else if (i % 3 == 0)
-		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-		}
-		else if (i % 5 == 0)
-		{
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-		}
-		else
-		{
-			_putchar(i + '0');
-		}
-
-		_putchar(' ');  /* Print space between numbers and words */
-	}
-
-	_putchar('\n');  /* Print newline at the end */
+    int i;
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            _putchar('F');
+        if (i % 3 == 0 && i % 5 == 0)
+            _putchar('i');
+        if (i % 3 == 0 && i % 5 == 0)
+            _putchar('z');
+        if (i % 3 == 0 && i % 5 == 0)
+            _putchar('z');
+        if (i % 5 == 0)
+            _putchar('B');
+        if (i % 5 == 0)
+            _putchar('u');
+        if (i % 5 == 0)
+            _putchar('z');
+        if (i % 3 != 0 && i % 5 != 0)
+            _putchar(i + '0');
+        _putchar(' ');
+    }
 }
