d943f8742a984f109980f09d05056e50189b72f9
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-02-23 20:56:57 +0000

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,41 @@
-#include "main.h"
+#include <stdio.h>
 
-void fizz_buzz(void); /* Declare the fizz_buzz function before main */
-
+/**
+ * main - prints numbers from 1 to 100
+ * for multiples of 3, prints Fizz
+ * for multiples of 5, prints Buzz
+ * for multiples of both 3 and 5, prints FizzBuzz
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    fizz_buzz();  /* Call the fizz_buzz function */
-    return 0;
-}
-
-void fizz_buzz(void)
-{
     int i;
+
     for (i = 1; i <= 100; i++)
     {
-        if (i % 3 == 0 && i % 5 == 0)
-            _putchar('F');
-        if (i % 3 == 0 && i % 5 == 0)
-            _putchar('i');
-        if (i % 3 == 0 && i % 5 == 0)
-            _putchar('z');
-        if (i % 3 == 0 && i % 5 == 0)
-            _putchar('z');
-        if (i % 5 == 0)
-            _putchar('B');
-        if (i % 5 == 0)
-            _putchar('u');
-        if (i % 5 == 0)
-            _putchar('z');
-        if (i % 3 != 0 && i % 5 != 0)
-            _putchar(i + '0');
-        _putchar(' ');
+        if (i % 3 == 0 && i % 5 == 0) /* Multiple of both 3 and 5 */
+        {
+            printf("FizzBuzz");
+        }
+        else if (i % 3 == 0) /* Multiple of 3 */
+        {
+            printf("Fizz");
+        }
+        else if (i % 5 == 0) /* Multiple of 5 */
+        {
+            printf("Buzz");
+        }
+        else /* Not a multiple of 3 or 5 */
+        {
+            printf("%d", i);
+        }
+
+        if (i != 100) /* Print space between numbers/words */
+        {
+            printf(" ");
+        }
     }
+    printf("\n"); /* New line after all output */
+    return (0);
 }
