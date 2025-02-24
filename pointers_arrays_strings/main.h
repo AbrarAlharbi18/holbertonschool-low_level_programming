8a2407aaa30f44f520dcd3bcab8f486b5cee8c76
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-02-24 06:31:08 +0000

    task 7

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
+/* Function Prototypes */
+void _putchar(char c);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
 void puts2(char *str);
-int _putchar(char c);
+void puts_half(char *str);
 
 #endif /* MAIN_H */
