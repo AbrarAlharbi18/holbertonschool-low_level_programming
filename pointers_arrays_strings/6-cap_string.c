feb91da776c014c8b2ac128c58c8a39328a4ce49
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-02-26 14:33:34 +0000

    task6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: input string
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int capitalize = 1; /* flag to check if the next letter should be capitalized */
+
+	while (str[i] != '\0')
+	{
+		if (capitalize && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 32; /* convert lowercase to uppercase */
+			capitalize = 0;
+		}
+		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '\"' ||
+				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+				str[i] == '}')
+		{
+			capitalize = 1; /* next letter should be capitalized */
+		}
+		else
+		{
+			capitalize = 0; /* continue in the middle of the word */
+		}
+		i++;
+	}
+	return (str);
+}
