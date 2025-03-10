b1e5acf9ac18767fc6470f306c0020a9421381a0
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-03-10 16:44:48 +0000

    task0

---
+++ more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h> /* For malloc and exit */
+
+/* Function prototype for malloc_checked */
+void *malloc_checked(unsigned int b);
+
+#endif /* MAIN_H */
