e0670af83b7be07cfd1d25ae20ad98fae2ac53cf
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-03-10 16:59:03 +0000

    task3

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -13,4 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 /* Function prototype for _calloc */
 void *_calloc(unsigned int nmemb, unsigned int size);
 
+/* Function prototype for array_range */
+int *array_range(int min, int max);
+
 #endif /* MAIN_H */
