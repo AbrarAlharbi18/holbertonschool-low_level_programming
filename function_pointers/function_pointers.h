f22f0cf9834ee9eced27f29db69642ea983b9722
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-03-16 20:56:17 +0000

    task1

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,12 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stddef.h>  /* For size_t (if needed) */
+#include <stddef.h>  /* For size_t */
 
 /* Function prototype for print_name */
 void print_name(char *name, void (*f)(char *));
+
+/* Function prototype for array_iterator */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
