d24bfeab2b318077fd26acb437d44580a2a18a4b
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-04-02 14:40:42 +0000

    task3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,82 +1,72 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-#define BUF_SIZE 1024
+#include "holberton.h"
 
 /**
- * handle_error - Prints error message and exits with code
- * @code: Exit code
- * @msg: Error message
- * @arg: Additional argument for message
- * @fd: File descriptor to close (if applicable)
+ * _errexit - print error message and exit
+ * @str: error message format
+ * @file: filename for error message
+ * @code: exit code
+ * @fd: file descriptor to close
  */
-void handle_error(int code, const char *msg, const char *arg, int fd)
+void _errexit(char *str, char *file, int code, int fd)
 {
 	if (fd != -1)
 		close(fd);
-	dprintf(STDERR_FILENO, msg, arg);
+	dprintf(STDERR_FILENO, str, file);
 	exit(code);
 }
 
 /**
- * copy_file - Copies content from one file to another
- * @fd_from: Source file descriptor
- * @fd_to: Destination file descriptor
- * @from_name: Source filename for error messages
- * @to_name: Destination filename for error messages
+ * _cp - copy source file to destination file
+ * @file_from: source file
+ * @file_to: destination file
  */
-void copy_file(int fd_from, int fd_to, const char *from_name,
-const char *to_name)
+void _cp(char *file_from, char *file_to)
 {
-	char buffer[BUF_SIZE];
-	ssize_t read_bytes, write_bytes;
+	int fd1 = -1, fd2 = -1;
+	ssize_t numread, numwrote;
+	char buffer[1024];
+	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	while (1)
+	fd1 = open(file_from, O_RDONLY);
+	if (fd1 == -1)
+		_errexit("Error: Can't read from file %s\n", file_from, 98, -1);
+
+	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, permissions);
+	if (fd2 == -1)
+		_errexit("Error: Can't write to %s\n", file_to, 99, fd1);
+
+	while ((numread = read(fd1, buffer, 1024)) > 0)
 	{
-		read_bytes = read(fd_from, buffer, BUF_SIZE);
-		if (read_bytes == -1)
-			handle_error(98, "Error: Can't read from file %s\n", from_name, fd_to);
-		if (read_bytes == 0)
-			break;
-		write_bytes = write(fd_to, buffer, read_bytes);
-		if (write_bytes != read_bytes)
-			handle_error(99, "Error: Can't write to %s\n", to_name, -1);
+		numwrote = write(fd2, buffer, numread);
+		if (numwrote != numread)
+			_errexit("Error: Can't write to %s\n", file_to, 99, fd1);
 	}
+
+	if (numread == -1)
+		_errexit("Error: Can't read from file %s\n", file_from, 98, fd2);
+
+	if (close(fd2) == -1)
+		_errexit("Error: Can't close fd %d\n", NULL, 100, fd1);
+
+	if (close(fd1) == -1)
+		_errexit("Error: Can't close fd %d\n", NULL, 100, -1);
 }
 
 /**
- * main - Copies content from one file to another
- * @argc: Argument count
- * @argv: Argument vector
+ * main - copies a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
  * Return: 0 on success
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int fd_from = -1, fd_to = -1;
-	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		handle_error(98, "Error: Can't read from file %s\n", argv[1], -1);
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permissions);
-	if (fd_to == -1)
-		handle_error(99, "Error: Can't write to %s\n", argv[2], fd_from);
-
-	copy_file(fd_from, fd_to, argv[1], argv[2]);
-
-	if (close(fd_from) == -1)
-		handle_error(100, "Error: Can't close fd %d\n", NULL, fd_to);
-
-	if (close(fd_to) == -1)
-		handle_error(100, "Error: Can't close fd %d\n", NULL, -1);
+	_cp(argv[1], argv[2]);
 
 	return (0);
 }
---
+++ file_io/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif /* HOLBERTON_H */
