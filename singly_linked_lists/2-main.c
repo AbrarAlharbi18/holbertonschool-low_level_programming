28f99f04f8ec68bbf1e228f5117e97c77e7b86fa
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-04-02 01:32:10 +0000

    task2

---
+++ singly_linked_lists/2-main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    list_t *head;
+
+    head = NULL;
+    add_node(&head, "Alexandro");
+    add_node(&head, "Asaia");
+    add_node(&head, "Augustin");
+    /* ... other add_node calls ... */
+    add_node(&head, "Zee");
+    print_list(head);
+    return (0);
+}
